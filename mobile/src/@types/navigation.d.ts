582eb89acdf08314f8424986183c54a5a5c2a3fc
Thiaggo Ferreira <thiaggo98@gmail.com>
2023-01-21 19:56:53 -0300

    Aula 3 completada e mobile no git

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+﻿export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            home: undefined;
+            new: undefined;
+            habit: {
+                date: string;
+            }
+        }
+    }
+}
---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,30 @@
+﻿import { createNativeStackNavigator} from '@react-navigation/native-stack'
+
+const {Navigator, Screen} = createNativeStackNavigator();
+
+import { Habit } from '../Screens/Habit';
+import { Home } from '../Screens/Home';
+import { New } from '../Screens/New';
+
+export function AppRoutes(){
+    return (
+        <Navigator screenOptions={{ headerShown: false}}>
+
+            <Screen
+                name='home'
+                component={Home}
+            />
+
+            <Screen
+                name='new'
+                component={New}
+            />
+
+            <Screen
+                name='habit'
+                component={Habit}
+            />
+
+        </Navigator>
+    )
+}
